bb9ddf2d66094fc186f24fc1012b39a4e47c409b
Hemanth-4301 <hemanthachar526@gmail.com>
2025-09-13 10:55:23 +0530

    fixed few bugs

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
-      origin: process.env.CLIENT_URL,
+      origin: [process.env.CLIENT_URL, "http://localhost:5173"],
       credentials: true,
     })
   );
